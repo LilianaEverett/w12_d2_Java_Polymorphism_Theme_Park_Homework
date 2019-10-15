9e6872dfdeefd258b01611558d22dbd1e5e91f8f
Liliana Everett <everettliliana@gmail.com>
2019-10-15 17:37:50 +0100

    rollercoaster has default price and pricefor visitor

--- src/main/java/attractions/RollerCoaster.java
+++ src/main/java/attractions/RollerCoaster.java
@@ -1,8 +1,28 @@
 package attractions;
 
-public class RollerCoaster  extends Attraction {
+import behaviours.ITicketed;
+import people.Visitor;
+
+public class RollerCoaster  extends Attraction implements ITicketed {
+
+    private double price;
 
     public RollerCoaster(String name, int rating) {
         super(name, rating);
+        this.price = 8.40;
+    }
+
+    @Override
+    public double defaultPrice(Visitor visitor) {
+        return this.price;
+    }
+
+    @Override
+    public double priceFor(Visitor visitor) {
+        if(visitor.getHeight() <= 200) {
+            return this.price;
+        } else {
+            return this.price * 2;
+        }
     }
 }
--- src/test/java/attractions/RollercoasterTest.java
+++ src/test/java/attractions/RollercoasterTest.java
@@ -2,16 +2,20 @@ package attractions;
 
 import org.junit.Before;
 import org.junit.Test;
+import people.Visitor;
 
 import static org.junit.Assert.assertEquals;
 
 public class RollercoasterTest {
 
     RollerCoaster rollerCoaster;
+    Visitor visitor;
 
     @Before
     public void setUp() {
+
         rollerCoaster = new RollerCoaster("Blue Ridge", 10);
+        visitor = new Visitor(35, 210, 50.00);
     }
 
     @Test
@@ -27,5 +31,15 @@ public class RollercoasterTest {
     @Test
     public void hasVisitCount() {
         assertEquals(0, rollerCoaster.getVisitCount());
+    }
+
+    @Test
+    public void hasDefaultPrice() {
+        assertEquals(8.40, rollerCoaster.defaultPrice(visitor), 0.01);
+    }
+
+    @Test
+    public void hasPricePriceForTallPeople() {
+        assertEquals(16.80, rollerCoaster.priceFor(visitor), 0.01);
     }
 }
